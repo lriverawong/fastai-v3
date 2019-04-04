5771e674d047da56559a71273a83311ca7e89daa
Luis Rivera-Wong <lriverawong@outlook.com>
2019-04-04 09:00:22 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1T_JzRiX563ZufzIjvNdyL4HUAR_dxz_S'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['MEL', 'NV', 'BCC', 'AKIEC', 'BKL', 'DF', 'VASC']
 path = Path(__file__).parent
 
 app = Starlette()
